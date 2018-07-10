1f06eb2c58ee4a88806e1b3f77c574816795515d
stephanie <stephanie99tech@gmail.com>
2018-07-09 23:08:52 -0700

    updated js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,19 +9,9 @@
         $('#losses').text(losses);  // using jquery prints the number 0 to screen
         $('#totalScore').text(total);  // using jquery prints the number 0 to screen
 
-        function youwin() {
-            document.querySelector('#updateMe').innerText = ('You Win!');
-            // or this code:
-            // var printyouwin = $('#wins');
-            // printyouwin.text('You win!');
-        }
-        function youLose() {
-            document.querySelector('#update').innerText = ('You lose!');
-            // or this code:
-            // var printloss = $('#losses');
-            // printloss.text('You lose!');
-        }
+        
      
+        //this CODE below WORKS ~ prints random number to pink box.
         function getRandomIntInclusive(min, max) {
           min = Math.ceil(min);
           max = Math.floor(max);
@@ -30,17 +20,27 @@
         }
         document.querySelector('#randomNumber').innerText = getRandomIntInclusive(19, 120);
         
+
         function getRandomGemNumber(min, max) {
             min = Math.ceil(min);
             max = Math.floor(max);
             return Math.floor(Math.random() * (max - min + 1)) + min; // random number generator 
         }
-        //document.querySelector('#totalScore').innerText = getRandomIntInclusive(1, 12);
+
+        
+            
+// test the #redgem code that prints out a random gem number to the blue box 'total score' for example
+// first click, the number 4 is generated. Then on second click & so on, the 'total score' is
+// incremented +4 each time:
+        var red = 0;
 
     	$('#redgem').on('click', function() {
-    	var gem = $(this);
-        console.log(getRandomGemNumber(1, 12)); // Player clicks on the red gem the random number is generated
-        document.querySelector('#totalScore').innerText = getRandomGemNumber(1, 12);
+        var gem = $(this);
+        var clicked = getRandomIntInclusive(1, 12);
+        red = clicked;
+        red++;
+        document.querySelector('#totalScore').innerText = red; 
+        
     });
 
         $('#yellowgem').on('click', function() {
@@ -58,24 +58,37 @@
         console.log(getRandomGemNumber(1, 12));
     });
 
+    function youwin() {
+        document.querySelector('#updateMe').innerText = ('You Win!');
+    }
+
+    function youLose() {
+        document.querySelector('#update').innerText = ('You lose!');
+    }
+
     if (totalScore == getRandomIntInclusive) {
-            youwin(); // calls the youWin() function from above
-            // wins++;  // increments wins +1
-            // arrayWins.push('wins'); // code to push win to array which can be retrieved later
-            // $('#wins').text(arrayWins); // should update wins to +1 (from array) & prints to screen
-            //$('#totalScore').text(total); //totalScore is back to 0
-            //document.querySelector('#randomNumber').innerText = getRandomIntInclusive(19, 120); 
+            youwin(); 
+            // calls the youwin() function above
+            wins++; 
+            // increments wins +1
+            document.querySelector('#wins').innerText = wins;
+            //<p><h4>Wins: <span id="wins"></span></h4></p>
+            
+            $('#totalScore').text(); 
+            document.querySelector('#randomNumber').innerText = getRandomIntInclusive(19, 120); 
             // new random number is generated and printed to screen
-            // ($('#losses').text(arrayLosses));  // code to print current losses from array
+            
         }
         
     if (totalScore < getRandomIntInclusive) {
-             youLose();  // calls the youLose() function from above
-    //         // losses++;  // increments losses to +1
-    //         // arrayLosses.push('losses');  // pushes losses to array which can be retrieved later
-    //         // $('#losses').text(arrayLosses);  // updates losses to +1 (from array) & prints to screen
-    //         //$('#totalScore').text(total); //totalScore is back to 0
-    //         //document.querySelector('#randomNumber').innerText = getRandomIntInclusive(19, 120); 
-    //         // new random number is generated and printed to screen
-    //         // $('#wins').text(arrayWins);  // code to print current wins from array    
-         }
+            youLose(); 
+            // calls the youLose() function above 
+            losses++;  
+            // increments losses to +1
+            document.querySelector('#losses').innerText = losses;
+            //<p><h4>Losses: <span id="losses"></span></h4></p>
+
+             $('#totalScore').text(); 
+             document.querySelector('#randomNumber').innerText = getRandomIntInclusive(19, 120); 
+            // new random number is generated and printed to screen
+           }
